d9a732e43fc13cdaa9ab56fbb30621ee07ff0794
Guillermo Lagras <guille_ivan@>
2023-05-14 22:17:58 -0300

    termine formulario, estilos y funcionalidad

--- src/pages/Formulario/index.jsx
+++ src/pages/Formulario/index.jsx
@@ -1,4 +1,4 @@
-import {useForm} from "react-hook-form";
+import { useForm } from "react-hook-form";
 import './formulario.css'
 
 
@@ -6,56 +6,53 @@ import './formulario.css'
 
 const Formulario = () => {
 
-    const { register, formState : {errors} , watch, handleSubmit }= useForm();
+    const { register, formState: { errors }, watch, handleSubmit } = useForm();
 
-    const onSubmit = (data)=> {
+    const onSubmit = (data) => {
         console.log(data);
     }
 
     const incluirtelefono = watch('incluirtelefono');
 
-    return(
+    return (
         <div className="divForm">
-            <h2>Formulario</h2>
-            <form  className='formularioContainer' action="" onSubmit={handleSubmit (onSubmit)}>
+            <h2>Formulario de registro</h2>
+            <form className='formularioContainer' action="" onSubmit={handleSubmit(onSubmit)}>
                 <div>
-                    <label>Nombre</label>
-                    <input type="text" {...register('nombre', {required:true} )} />
+                    <input type="text"  placeholder="Ingrese su nombre" className="labelForm" {...register('nombre', { required: true })} />
                     {errors.nombre?.type === 'required' && <p>El campo nombre es quererido</p>}
                 </div>
                 <div>
-                    <label>Apellido</label>
-                    <input type="text" {...register('apellido', {required:true})}/>
+                    <input type="text" placeholder="Ingrese su Apellido" className="labelForm"{...register('apellido', { required: true })} />
                     {errors.apellido?.type === 'required' && <p>El campo apellido es quererido</p>}
                 </div>
                 <div>
-                    <label>Direccion</label>
-                    <input type="text" {...register('direccion', {required:true})}/>
+                    <input type="text" placeholder="Ingrese su Direccion" className="labelForm" {...register('direccion', { required: true })} />
                     {errors.direccion?.type === 'required' && <p>El campo direccion es quererido</p>}
                 </div>
                 <div>
-                    <label>Edad</label>
-                    <input type="text" {...register('edad', {required:true})}/>
+                    <input type="text" placeholder="Ingrese su edad" className="labelForm" {...register('edad', { required: true })} />
                     {errors.edad?.type === 'required' && <p>El campo Edad es quererido</p>}
                 </div>
                 <div>
-                    <label>Incluir telefono?</label>
-                    <input type="checkbox" {...register('incluirtelefono')}/>
+                    <label className="formTelefono">Incluir telefono?</label>
+                    <input type="checkbox" placeholder="Ingrese su Telefono" {...register('incluirtelefono')} />
                 </div>
                 {incluirtelefono && (
                     <div>
-                    <label>telefono?</label>
-                    <input type="text" {...register('telefono')}/>
-                </div>
+                        <input type="text" className="labelForm" placeholder="Ingrese su telefono"{...register('telefono')} />
+                    </div>
                 )}
-                
+
                 <div>
-                    <label>E-mail</label>
-                    <input type="email" id="" {...register('email')}/>
+                    <input type="email" id="" className="labelForm" placeholder="Ingrese su E-mail"{...register('email')} />
                 </div>
                 <div>
-                    <label>Pais</label>
-                    <select  {...register('pais')}>
+                        <input type="password" className="labelForm" placeholder="Ingrese su contraseña"{...register('password')} />
+                    </div>
+                <div>
+                    <label className="formPais">Seleccione su Pais</label>
+                    <select className="labelForm"  {...register('pais')}>
                         <option value="ar">Argentina</option>
                         <option value="ch">Chile</option>
                         <option value="br">Brasil</option>
@@ -63,7 +60,10 @@ const Formulario = () => {
                         <option value="ve">Venezuela</option>
                     </select>
                 </div>
-                <input type="submit" value="Enviar" />
+                <input type="submit" className="botonEnviar" value="Enviar" />
+                <p>
+                    <a href="#">¿Ya tienes una cuenta ?</a>
+                </p>
             </form>
         </div>
     );
